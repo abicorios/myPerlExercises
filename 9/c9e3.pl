83a143a2400d2660a7aaaa820efc080dad606dc0
abicorios <yaroslav.moroz91@gmail.com>
2018-10-11 14:54:43 +0300

    simplification

--- 9/c9e3.pl
+++ 9/c9e3.pl
@@ -6,9 +6,8 @@ $fn=pop @ARGV;
 open F,'<',$fn;
 open O,'>',$fn.'.out';
 while(<F>){
-	s/fred/\L$&!/ig;
-	s/wilma/\L$&!/ig;
-	s/fred!/Wilma/g;
-	s/wilma!/Fred/g;
+	s/fred|wilma/\L$&/ig;
+	s/fred/Wilma/g;
+	s/wilma/Fred/g;
 	print O;
 }
